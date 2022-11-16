8fd9ceb829ce85659f52e1c268c51968854f1446
Jimpa Jamtsho <jamtshojimpa@gmail.com>
2022-11-16 10:03:07 +0530

    first commit

---
+++ Driver_Registration/DriverReg.java
@@ -0,0 +1,10 @@
+package Driver_Registration;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class DriverReg{
+	
+}
---
+++ Mini_Project.java
@@ -0,0 +1,12 @@
+//Importing the packagees
+import Vehicle_and_Travel_Details.*;
+import Driver_Registration.*;
+import Passenger_Registration.*;
+
+public class Mini_Project{
+	public static void main(String args[]){
+		//Creating an object for a package class 
+		Vehicle_Travel_Details vtd=new Vehicle_Travel_Details();
+	}
+	
+}
